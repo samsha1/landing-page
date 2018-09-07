b8a59c758b60630800642f088074a7004be5a0b5
samrat <samrat.shakya@nepallink.net>
2018-09-07 16:37:50 +0545

    about page begined

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,17 +1,15 @@
 import React from 'react';
-
+import  './styles.css';
 class AboutPage extends React.Component{
-
 	render(){
 		return(
-				<div>
-					<h1>About Samrat</h1>
+				<div className="container aboutTitle">
+					<h1> ABOUT </h1>		
 				</div>
 
 			)
 	}
 
 }
-
 
 export default AboutPage;
