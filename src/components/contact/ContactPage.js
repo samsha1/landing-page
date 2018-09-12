f9361c5a1727856ee47582413131c28617df2852
samrat <samrat.shakya@nepallink.net>
2018-09-12 17:34:31 +0545

    contact form ui implemented

--- src/components/contact/ContactPage.js
+++ src/components/contact/ContactPage.js
@@ -1,14 +1,72 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import ContactForm from './ContactForm';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
 
-const ContactPage = () => {
-	return (
-		<div className="row">
-			<div className="col-6">
-				<h2>Contact Page</h2>
-			</div>
-		</div>
+import Paper from '@material-ui/core/Paper';
 
-		);
+const styles = theme => ({
+root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 3,
+    paddingBottom: theme.spacing.unit * 3,
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    minWidth: 300,
+    maxWidth: 400,
+    height: 'auto',
+    position: 'absolute',
+    top: '20%',
+    left: 10,
+    right: 0,
+    margin: 'auto'
+  },
+  margin: {
+    margin: theme.spacing.unit,
+    zIndex:1000
+  },
+  cssLabel: {
+    '&$cssFocused': {
+      color: purple[500],
+    },
+  },
+  cssFocused: {},
+  cssUnderline: {
+    '&:after': {
+      borderBottomColor: purple[500],
+    },
+  },
+});
+
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+  },
+});
+
+class ContactPage extends React.Component {
+
+
+render(){
+	const { classes } = this.props;
+  return (
+    <div className={classes.container}>
+    <Paper className={classes.root} elevation={1}>
+    <MuiThemeProvider theme={theme}>
+    	<ContactForm classes={classes} maxCharacter={280}/>
+    </MuiThemeProvider>   
+     </Paper>
+    </div>
+  );
+  }
 }
 
-export default ContactPage;
+ContactPage.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ContactPage);
