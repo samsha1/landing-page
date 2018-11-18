836cd84ad49de87afdf41fc3d56e2124847d7404
samrat <samratshakya5@gmail.com>
2018-11-18 21:57:16 +0545

    just edited aboutPage intro

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -20,7 +20,7 @@ class AboutPage extends React.Component{
 								<div className="col-md-7">
 									<div className="about-content">
 										<p>Hi, I'm samrat and I like everything web. I still remember my first C 'Hello World' seven years ago. It was never stopping loop. Life has become same now, 
-											Since then I never stopped to write code.It was a beauty of moving line console code. Graduated, Bachelor in Computer Science and Information Technology in 2018. It was my college days, where I jumped to web Development and PHP in backend and JAVASCRIPT to frontend became my friends. I used to date each library and framework.</p>
+											Since then I never stopped to write code.It was a beauty of moving line console code. Graduated, Bachelor in Computer Science and Information Technology in 2018. It was my college days, when I jumped to web Development and PHP in backend and JAVASCRIPT to frontend became my friends. I used to date each library and framework.</p>
 										<p>I'm (mostly) a self taught Software Engineer. My favourite stack right now is React JS on the frontend and Laravel on the backend. I have also worked with Go, WP MVC, JQuery, MongoDB, MYSQL, Docker, CodeIgniter. I am also obsessed with making the web look pretty with SASS/CSS.</p>
 										<p>When I am not working you can find me watching or playing Football, reading and writing blogs, Travelling, Researching in Open Source Projects.</p>
 									</div>
